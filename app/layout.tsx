2de27151af9f831dc6e8f4d4b92f1e8a9a12bb44
Matt Pierringer <pierrim@amazon.com>
2024-11-03 05:31:58 -0800

    added authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
+import { Amplify } from "aws-amplify";
 import { Inter } from "next/font/google";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "@/amplify_outputs.json";
 import "./app.css";
+
+Amplify.configure(outputs);
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +20,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+  return (<html lang="en">
+    <body>      
+      <Authenticator>
+        {children}
+      </Authenticator>
+    </body>
+  </html>)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -7,6 +7,8 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+
+  const { signOut } = useAuthenticator();
 
 Amplify.configure(outputs);
 
@@ -31,13 +33,17 @@ export default function App() {
     });
   }
 
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
         ))}
       </ul>
       <div>
@@ -47,6 +53,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
